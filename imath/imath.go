3663345dbcf1edfe873493049cfca6e1ddcc7cd1
0xmask <samirlol.shao@gmail.com>
2024-08-13 15:41:37 +0800

    v0.0.5

--- imath/imath.go
+++ imath/imath.go
@@ -39,21 +39,7 @@ func RandStr(n int) string {
 
 // Decimal 保留n位小数
 func Decimal(value float64, length int) (res float64) {
-	switch length {
-	case 1:
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.1f", value), 64)
-	case 2:
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.2f", value), 64)
-	case 3:
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.3f", value), 64)
-	case 4:
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.4f", value), 64)
-	case 5:
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.5f", value), 64)
-	case 6:
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.6f", value), 64)
-	default:
-		res = 0
-	}
+	args := fmt.Sprintf("%%.%df", length)
+	res, _ = strconv.ParseFloat(fmt.Sprintf(args, value), 64)
 	return res
 }
