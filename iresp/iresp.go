d6e2067b62e78395097a451fc6641767879108f3
0xmask samir <samir@0xmaskdeMacBook-Air.local>
2024-10-31 16:00:05 +0800

    update iresp

--- iresp/iresp.go
+++ iresp/iresp.go
@@ -1,8 +1,6 @@
 package iresp
 
 import (
-	"encoding/json"
-	"fmt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -16,49 +14,36 @@ type Message struct {
 	Data   interface{} `json:"data"`   //响应数据
 }
 
-// StatusError rpc错误格式化
-func StatusError(code int, msgArr ...string) error {
-	var msg string
-	if len(msgArr) > 0 {
-		msg = msgArr[0]
-	}
-	if code > 0 {
-		msg = fmt.Sprintf("错误代码%d %s", code, msg)
-	}
-	enc, _ := json.Marshal(&Message{
-		Status: 0,
-		Code:   code,
-		Msg:    msg,
-		Data:   nil,
-	})
-	return status.Errorf(codes.Internal, string(enc))
-}
-
-// Error 格式化错误相应
-// err 错误信息，支持一般错误和rpc格式化错误，需要配合resp.StatusError使用
-func Error(code int, errs ...error) Message {
+func GrpcError(code int, errs ...error) Message {
 	var errMsg interface{}
 	var errCode = code
 	if len(errs) > 0 {
 		sta := status.Convert(errs[0])
-		if sta.Code() == codes.Unknown {
-			if Debug == true {
-				errMsg = errs[0].Error()
-			}
-		} else {
-			var data Message
-			if err := json.Unmarshal([]byte(sta.Message()), &data); err != nil {
-				errMsg = "unknow error"
-			} else {
-				errCode = data.Code
-				errMsg = data.Msg
-			}
+		errMsg = sta.Message()
+		if sta.Code() != codes.OK {
+			errCode = int(sta.Code())
 		}
 	}
 
 	return Message{
 		Status: 0,
 		Code:   errCode,
+		Msg:    errMsg,
+		Data:   nil,
+	}
+}
+
+// Error 格式化错误相应
+// err 错误信息，支持一般错误和rpc格式化错误，需要配合resp.StatusError使用
+func Error(code int, errs ...error) Message {
+	var errMsg interface{}
+	if len(errs) > 0 {
+		errMsg = errs[0].Error()
+	}
+
+	return Message{
+		Status: 0,
+		Code:   code,
 		Msg:    errMsg,
 		Data:   nil,
 	}
@@ -77,13 +62,4 @@ func Success(code int, data ...interface{}) Message {
 		Msg:    "success",
 		Data:   respData,
 	}
-}
-
-// 反解析
-func ParseData(src []byte) (Message, error) {
-	var data Message
-	if err := json.Unmarshal(src, &data); err != nil {
-		return Message{}, err
-	}
-	return data, nil
 }
